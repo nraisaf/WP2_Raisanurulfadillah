e2a4e7e040c20bc1803b3c1fc37a64c72b4eaed6
nraisaf <nraisaaf@gmail.com>
2021-09-19 20:48:33 +0700

    Menambahkan file Codeigniter dan Contoh1

--- Pustaka-booking/application/controllers/Contoh1.php
+++ Pustaka-booking/application/controllers/Contoh1.php
@@ -4,9 +4,10 @@ class Contoh1 extends CI_Controller
     public function index()
     { 
         echo "<h1>Perkenalkan</h1>";
-        echo"Nama saya Imam Nawawi
-            Saya tinggal di daerah Ciputat
-            olah raga yang saya sukai adalah
-            Bulutangkis";
+        echo"Nama saya Raisa Nurul Fadillah
+            Saya tinggal di daerah Sukaraja
+            olahraga dan termasuk hobi saya 
+            adalah berkegiatan di outdoor 
+            seperti mendaki, berenang dan camping";
     }
 }
