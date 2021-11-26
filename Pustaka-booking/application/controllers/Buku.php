225100c411fc57863b3fb597d8c27aa2a6683035
nraisaf <nraisaaf@gmail.com>
2021-11-26 21:01:54 +0700

    Menambahkan File ubahkategori.php dan revisi Buku.php

--- Pustaka-booking/application/controllers/Buku.php
+++ Pustaka-booking/application/controllers/Buku.php
@@ -1,4 +1,6 @@
 <?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
 class Buku extends CI_Controller
 {
     public function __construct()
@@ -34,5 +36,29 @@ class Buku extends CI_Controller
         $where = ['id' => $this->uri->segment(3)];
         $this->ModelBuku->hapusKategori($where);
         redirect('buku/kategori');
+    }
+
+    public function ubahKategori()
+    {
+        $data['judul'] = 'Ubah Data Buku';
+        $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
+        $data['kategori'] = $this->ModelBuku->kategoriWhere(['id' => $this->uri->segment(3)])->result_array();
+
+        $this->form_validation->set_rules('kategori', 'Nama Kategori', 'required|min_length[3]', [
+            'required' => 'Nama Kategori harus diisi',
+            'min_length' => 'Nama terlalu pendek'
+        ]);
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('buku/ubahkategori', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $data = ['kategori' => $this->input->post('kategori', true)];
+            $this->ModelBuku->UpdateKategori(['id' => $this->input->post('id')], $data);
+            redirect('buku/kategori');
+        }
     }
 }
